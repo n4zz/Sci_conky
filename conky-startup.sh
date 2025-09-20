c0149e62bf594ed0320416a60bda4749b3cde70c
n4zz <160547375+n4zz@users.noreply.github.com>
2025-09-20 04:11:59 +0200

    Add files via upload

--- conky-startup.sh
+++ conky-startup.sh
@@ -2,28 +2,27 @@
 killall conky
 sleep 5s
 
-
-cd "$HOME/.config/conky/robust2/"
+cd "$HOME/.config/conky/Sci_conky-main/"
 conky -q -c time.conf &
 
-cd "$HOME/.config/conky/robust2/"
+cd "$HOME/.config/conky/Sci_conky-main/"
 conky -q -c date.conf &
 
-cd "$HOME/.config/conky/robust2/"
+cd "$HOME/.config/conky/Sci_conky-main/"
 conky -q -c cal.conf &
 
-cd "$HOME/.config/conky/robust2/"
+cd "$HOME/.config/conky/Sci_conky-main/"
 conky -q -c sys.conf &
 
-cd "$HOME/.config/conky/robust2/"
+cd "$HOME/.config/conky/Sci_conky-main/"
 conky -q -c net.conf &
 
-cd "$HOME/.config/conky/robust2/"
+cd "$HOME/.config/conky/Sci_conky-main/"
 conky -q -c hw.conf &
 
-cd "$HOME/.config/conky/robust2/"
+cd "$HOME/.config/conky/Sci_conky-main/"
 conky -q -c weather.conf &
 
-exit 1
+exit 0
 
 
